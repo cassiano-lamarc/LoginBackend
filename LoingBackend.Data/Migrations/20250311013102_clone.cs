d8d9ce6ed3378f0487c8a39ea14148e52ddd52af
cassiano-lamarc <cacassiano2011@gmail.com>
2025-03-10 22:31:50 -0300

    fixing projects

---
+++ LoingBackend.Data/Migrations/20250311013102_clone.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoingBackend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class clone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
